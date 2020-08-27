f23d287625c0bd5696082323029eb91233455561
shakedAviad <shakedaviad50@gmail.com>
2020-08-27 12:57:08 +0300

    create DGraph

---
+++ Ex2/code/Gui/GraphGui.java
@@ -0,0 +1,46 @@
+package Gui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.KeyStroke;
+import javax.swing.ListSelectionModel;
+import javax.swing.SpinnerListModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+import algorithms.Graph_Algo;
+import dataStructure.DGraph;
+import dataStructure.Node;
+import dataStructure.graph;
+import dataStructure.edge_data;
+import dataStructure.node_data;
+import utils.Point3D;
+public class GraphGui {
+
+}
--- Ex2/code/dataStructure/DGraph.java
+++ Ex2/code/dataStructure/DGraph.java
@@ -1,73 +1,211 @@
 package dataStructure;
 
+import utils.Point3D;
+
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.io.Serializable;
 
-public class DGraph implements graph{
 
+public class DGraph implements graph,Serializable{
+	private int nodesCount;
+	private int edgesCount;
+	private int modeCount;
+	public HashMap<Integer,node_data> nodeGraph;
+	public HashMap<Integer,HashMap<Integer,edge_data>> edgeGraph;
+	
+	/**
+	 * Default constructor
+	 */
+	public DGraph(){
+		this.nodeGraph = new HashMap<Integer,node_data>();
+		this.edgeGraph = new HashMap<Integer,HashMap<Integer,edge_data>>();
+		this.nodesCount =0;
+		this.edgesCount =0;
+		this.modeCount =0;
+	}
 	@Override
 	public node_data getNode(int key) {
-
-		return null;
+		try{
+			return this.nodeGraph.get(key);
+		}
+		catch (Exception e){
+			throw new RuntimeException ("The Node " + key + " NOT exist");
+		}
 	}
-
+	/**
+	 * @param src First tip in the edge
+	 * @param dest Second tip in the edge
+	 * @return A Edge that starts at the src vertex and ends at the vertex dest
+	 * We will check if the Edge is exsit, if yes - we return it, otherwise we will throw an exception
+	 * that stops the program and notify the user that no such Edge exists.
+	 */
 	@Override
 	public edge_data getEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			return this.edgeGraph.get(src).get(dest);
+		}
+		catch (Exception e){
+			System.out.println("The Edge " + src + " - " + dest + " NOT exist");
+			return null;
+		}
 	}
-
+	/**
+	 * @param n The Node we want added to the Dgraph
+	 * We add the Node to the graph, if this node already exists in the graph we will replace him with the new node.
+	 * we advance nodesCount and modeCount
+	 */
 	@Override
 	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
+		if (this.nodeGraph.get(n.getKey())!=null){ // if Node n exsist in the graph
+			HashMap<Integer,edge_data> src = new HashMap<Integer, edge_data>();
+			if (this.edgeGraph.get(n.getKey())!=null) { // if there is edge that start at Node n
+				src = this.edgeGraph.get(n.getKey());
+			}
+			HashMap <Integer,Double> dest =new HashMap<Integer, Double>();
+			Iterator iter = this.edgeGraph.entrySet().iterator();
+			while (iter.hasNext()) { //add all the edge that look like x -> node
+				Map.Entry mapElement = (Map.Entry) iter.next();
+				int currSrc = ((int) mapElement.getKey());
+				if (this.edgeGraph.get(currSrc).get(n.getKey()) != null)
+					dest.put(currSrc,this.edgeGraph.get(currSrc).get(n.getKey()).getWeight());
+			}
+			removeNode(n.getKey());
+			this.nodeGraph.put(n.getKey(),n);
+			this.edgeGraph.put(n.getKey(),src);
+			if (dest!= null){
+				for (Integer i : dest.keySet()){
+					connect(i,n.getKey(),dest.get(i));
+				}
+			}
+		}
+		else{
+			this.nodesCount++;
+			this.nodeGraph.put(n.getKey(),n);
+		}
+		this.modeCount++;
 		
 	}
-
+	/**
+	 * @param src - the source of the edge.
+	 * @param dest - the destination of the edge.
+	 * @param w - positive weight representing the cost (aka time, price, etc) between src-->dest.
+	 * The function connects a new edge to the graph
+	 */
 	@Override
 	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
+		if(w<0)
+		{
+			throw new RuntimeException("ERR: The weight is negative");
+		}
+		//Checks that both vertices exist
+		if (this.nodeGraph.get(src)!=null && this.nodeGraph.get(dest)!=null) {
+			edge_data newEdge = new Edge(src, dest, w);
+			if (this.edgeGraph.get(src) != null) { //If src has neighbors
+				if (this.edgeGraph.get(src).get(dest) == null) { //the edge not exists
+					this.edgeGraph.get(src).put(dest, newEdge);
+				} else {
+					removeEdge(src, dest);
+					this.edgeGraph.get(src).put(dest, newEdge);
+				}
+			} else { // no hash map meaning src has no neighbors
+				HashMap<Integer, edge_data> toAdd = new HashMap<Integer, edge_data>();
+				this.edgeGraph.put(src, toAdd);
+				this.edgeGraph.get(src).put(dest, newEdge);
+			}
+			this.edgesCount++;
+			this.modeCount++;
+		}
+		else System.out.println("ERR: One of the Nodes does not exist");
 		
 	}
-
+	/**
+	 * @return Collection that representing the Nodes in the graph
+	 */
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return this.nodeGraph.values();
 
+	}
+	/**
+	 * @param node_id
+	 * @return Collection that representing the edges in the graph
+	 */
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			return this.edgeGraph.get(node_id).values();
+		}
+		catch (NullPointerException e){
+			return null;
+		}
 	}
-
+	/**
+	 * @param key
+	 * @return the node we removed
+	 * Deleting a node from the graph,
+	 * note that When the node is deleted, all the edge containing the node are also deleted.
+	 */
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		node_data toReturn = this.nodeGraph.get(key);
+		if (toReturn!=null) {
+			this.nodeGraph.remove(key); //remove node from nodeGraph
+			if (this.edgeGraph.get(key) != null) this.edgeGraph.remove(key); //remove all the edge that look like node -> x
+			Iterator iter = this.edgeGraph.entrySet().iterator();
+			while (iter.hasNext()) { //remove all the edge that look like x -> node
+				Map.Entry mapElement = (Map.Entry) iter.next();
+				int currSrc = ((int) mapElement.getKey());
+				if (this.edgeGraph.get(currSrc).get(key) != null) removeEdge(currSrc, key);
+			}
+			this.nodesCount--;
+			this.modeCount++;
+		}
+		return toReturn;
 	}
-
+	/**
+	 * @param src
+	 * @param dest
+	 * @return the edge we removed
+	 */
 	@Override
 	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		edge_data toReturn = this.edgeGraph.get(src).get(dest);
+		if (toReturn!=null) {
+			this.edgeGraph.get(src).remove(dest);
+			this.edgesCount--;
+			this.modeCount++;
+		}
+		return toReturn;
 	}
 
 	@Override
 	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.nodesCount;
 	}
 
 	@Override
 	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.edgesCount;
 	}
 
 	@Override
 	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.modeCount;
 	}
-
+	/**
+	 * @return next valid key number that not used in this graph
+	 * Proper use of the function will be done by immediate adding the node to the graph after it is created,
+	 * otherwise a situation where double use of the function without instant adding will return the same value to
+	 * 2 different nodes that override each other which can cause the entire graph to be disrupted
+	 */
+	public int findNextKey(){
+		int i = 1;
+		while (this.nodeGraph.get(i)!=null){
+			i++;
+		}
+		return i;
+	}
 }
